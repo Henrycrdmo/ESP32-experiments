e39547d0404d0ea0c1408f01f6b3503f8c5eaef1
Henrycrdmo <henrique.crdmorais@gmail.com>
2025-10-23 16:32:48 -0300

    Working MAX30102 code

--- MAX30102/main/main.c
+++ MAX30102/main/main.c
@@ -7,34 +7,55 @@
 #include "i2c_api.h"
 #include "max30102_api.h"
 
+TaskHandle_t processor_handle = NULL;
+TaskHandle_t sensor_reader_handle = NULL;
+
+int32_t red_data = 0;
+int32_t ir_data = 0;
+int32_t red_data_buffer[BUFFER_SIZE];
+int32_t ir_data_buffer[BUFFER_SIZE];
+double auto_correlationated_data[BUFFER_SIZE];
+
+char *data = NULL;
+
+#define DELAY_AMOSTRAGEM 40
+
+#define DEBUG true
+
 static const char *TAG = "PPG_MAIN";
 
 void app_main(void){
-    ESP_LOGI(TAG, "Starting MAX30102 sensor...");
+    
+    #if DEBUG
+        ESP_LOGI(TAG, "Initialising MAX30102 sensor...");
+    #endif
 
     // Initialise the I2C bus
     esp_err_t ret = i2c_init();
-    if(ret != ESP_OK){
-        ESP_LOGE(TAG, "I2C init failed: %s", esp_err_to_name(ret));
-        return;
-    }
-    ESP_LOGI(TAG, "I2C initialised successfully");
+
+    #if DEBUG
+        if(ret != ESP_OK){
+            ESP_LOGE(TAG, "I2C init failed: %s", esp_err_to_name(ret));
+            return;
+        }
+        ESP_LOGI(TAG, "I2C initialised successfully");
+    #endif
 
     // Initialise MAX30102 (configs handled inside the MAX driver)
     max30102_init(NULL);
-    ESP_LOGI(TAG, "MAX30102 initialised and configured");
 
-    // 3. Read FIFO and log data
-    int32_t red = 0, ir = 0;
+    #if DEBUG
+        ESP_LOGI(TAG, "MAX30102 initialised and configured");
+    #endif
 
     for(;;)
     {
-        if (max30102_read_fifo(I2C_NUM_0, &red, &ir) == ESP_OK){   // Checks I2C connection
-            ESP_LOGI(TAG, "RED: %ld | IR: %ld", red, ir);		   // Reads data
+        if (max30102_read_fifo(I2C_NUM_0, &red_data, &ir_data) == ESP_OK){   // Checks I2C connection
+            ESP_LOGI(TAG, "RED: %ld | IR: %ld", red_data, ir_data);		   // Reads data
         } else {
             ESP_LOGW(TAG, "Failed to read FIFO");
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay (reads every second)
+        vTaskDelay(pdMS_TO_TICKS(100)); // Delay (reads every 1/10 second)
     }
 }
